c10bbce541624be2d5b80e97a244e869efaf47da
niedson <niofernandodf@gmail.com>
2023-11-09 16:28:51 -0300

    :rocket: first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logo from '../../assets/logo.svg'
+import * as  Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logo} alt="" />
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    <NewTransactionModal />
+                </Dialog.Root>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,35 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { X, ArrowCircleUp, ArrowCircleDown } from "phosphor-react"
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal>
+            <Overlay />
+            <Content>
+                <CloseButton >
+                    <X size={24} />
+                </CloseButton>
+                <Dialog.Title>
+                    Nova transação
+                </Dialog.Title>
+                <form action="">
+                    <input type="text" placeholder="Descrição" required />
+                    <input type="number" placeholder="Preço" required />
+                    <input type="text" placeholder="Categoria" required />
+                    <TransactionType>
+                        <TransactionTypeButton variant="income" value="income">
+                            <ArrowCircleUp size={24} />
+                            Entrada
+                        </TransactionTypeButton>
+                        <TransactionTypeButton variant="outcome" value="outcome">
+                            <ArrowCircleDown size={24} />
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
+                    <button type="submit">Cadastrar</button>
+                </form>
+            </Content>
+        </Dialog.Portal>
+    )
+}
